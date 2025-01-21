32cd7304b27fcb46c1fdad87acdd310ef6c5947e
chirag jariya <143921296+chiragpjariya@users.noreply.github.com>
2025-01-21 15:31:07 +0530

    ..

--- sagments/src/app/login/[...loginSame]/page.js
+++ sagments/src/app/login/[...loginSame]/page.js
@@ -1,8 +1,23 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect } from 'react'
 
 function page() {
+
+
+    // async function demo() {
+    //     const unwrappedParams = await params; // Unwrap the params Promise
+    //     const { loginSame } = unwrappedParams;
+    //     console.log(loginSame);
+    // }
+
+    // useEffect(() => {
+    //     demo()
+    // })
+
+
     return (
-        <div>
+        <div className='flex items-center justify-center h-screen text-[20px]'>
             some-thing wrong
         </div>
     )
