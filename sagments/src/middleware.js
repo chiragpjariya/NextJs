528056d89a82a35898f0eddd6de5e0e5e858aaf3
chirag jariya <143921296+chiragpjariya@users.noreply.github.com>
2025-01-21 18:04:33 +0530

    ..

--- sagments/src/middleware.js
+++ sagments/src/middleware.js
@@ -1,4 +1,13 @@
-export  function middleware(){
-    console.log('middleware');
+import { NextResponse } from "next/server";
+
+export  function middleware(request){
+
+    // if(request.nextUrl.pathname != '/login'){
+    console.log(request);
     
+        return NextResponse.redirect(new URL('/login', request.nextUrl));
+    // }
+}
+export const config= {
+    matcher:'/about/...not-found/:path*'
 }
